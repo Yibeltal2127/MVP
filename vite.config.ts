8ee73a6147f105ad1cbfed43b39135f902c78103
Yibeltal2127 <ebabuyibeltal3@gmail.com>
2025-05-20 12:22:46 +0300

    Fix Vite Preview Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
